b7a5c2ca3873009bb19b04ba302e261bf117c686
pranav550 <pranavverma550@gmail.com>
2020-03-20 17:20:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { ApiServiceService } from './services/api-service.service';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angualr1';
+  error: String;
+  data: any = []
+
+  constructor(private apiService: ApiServiceService) {
+
+  }
+
+  dataGet() {
+    this.apiService.getData().subscribe(data => {
+      console.log(data)
+      this.data = data
+    }, (error) => {
+      console.log(error)
+      this.error = error
+    })
+
+  }
 }
